1459595db23b4b2d4635b600b817b95c0662550f
Ninte <ninte.dev@gmail.com>
2021-07-24 17:29:32 +0100

    Setup markdown editor & preview functionality.

---
+++ src/previewer/Editor.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import marked from 'marked';
+import html from 'html-react-parser';
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      input: e.target.value,
+    });
+  }
+
+  render() {
+    const rawHtml = marked(this.state.input);
+    const parsedHtml = html(rawHtml);
+
+    return (
+      <div id="board">
+        <textarea
+          id="editor"
+          rows="20"
+          cols="10"
+          value={this.state.input}
+          onChange={this.handleInput}></textarea>
+        <br />
+        <p id="preview">{parsedHtml}</p>
+      </div>
+    );
+  }
+}
+
+export default Editor;
--- src/previewer/index.js
+++ src/previewer/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Editor from './Editor';
 
 function Previewer() {
   return (
     <div>
       <h1 id="title">Markdown Previewer</h1>
+      <Editor />
     </div>
   );
 }

18aa3f6401e49a191d009f9ee9376c72f86da655
Ninte <ninte.dev@gmail.com>
2021-07-24 17:32:43 +0100

    Implemented needed packages for parsing input.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import ReactFCCtest from 'react-fcctest';
+
 import './App.css';
 import Previewer from './previewer';
 
 function App() {
   return (
     <div className="app">
+      <ReactFCCtest />
       <Previewer />
     </div>
   );

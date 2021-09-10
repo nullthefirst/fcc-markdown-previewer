e9042734285f5f42b20e63ffa2900f508774a3cd
nullthefirst <ninte.dev@gmail.com>
2021-09-10 08:12:32 +0100

    Moved footer component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Previewer from "./previewer";
+import Footer from "./previewer/Footer";
 
 function App() {
   return (
     <div>
       <Previewer />
+      <Footer />
     </div>
   );
 }
